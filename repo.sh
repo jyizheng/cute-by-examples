8f04a518843d38261db0f97e0a2b119af73137c4
jyizheng <jyizheng@gmail.com>
2025-08-27 22:22:46 +0000

    add cutlass headers

---
+++ repo.sh
@@ -0,0 +1,9 @@
+echo "# cute-by-examples" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:jyizheng/cute-by-examples.git
+git push -u origin main
+
+
